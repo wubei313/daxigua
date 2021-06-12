362c96744b903b66c3d674664c055a1ac228764d
wubei313 <wubei313@qq.com>
2021-06-12 11:51:02 +0800

    add build file

---
+++ src/settings.71539.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","testGroup"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"eb282"}};
