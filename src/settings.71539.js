7b390fcef48cafe93e664140a6df2ad7d07f24ee
Ben <46591529+Ben-indoing@users.noreply.github.com>
2021-06-12 14:41:32 +0800

    delete no useful file

--- src/settings.71539.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","testGroup"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"eb282"}};
